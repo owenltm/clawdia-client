b9cbda0a28dfac6e03c8c7a13f299b3b729ddf96
owenltm <owenlwiantoro01@gmail.com>
2025-10-29 14:46:46 +0800

    move finance data fetch to client side

--- src/app/(protected)/(main)/finance/FinancePage.tsx
+++ src/app/(protected)/(main)/finance/FinancePage.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import { Button } from "@/components/Button";
-import FinanceDataTable from "@/components/ui/finances/financeDataTable";
-import { FinanceFormModal } from "@/components/ui/finances/FinanceFormModal";
-import { Finance, FinanceFormValues } from "@/types/finance.types";
-import { RiAddLine } from "@remixicon/react";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { Finance } from "@/types/finance.types";
 
 type FinancePageProps = {
   finances: Finance[],
@@ -15,57 +9,6 @@ type FinancePageProps = {
   onDelete: (id: number) => Promise<boolean>,
 }
 
-export default function FinancePage({
-  finances,
-  onEdit,
-  onDelete,
-  onSubmit
-}: FinancePageProps) {
-  const router = useRouter();
-  const [focusedFinance, setFocusedFinance] = useState<FinanceFormValues | {}>({});
+export default function FinancePage(_params: FinancePageProps) {
 
-  const onFocusFinance = (id: number) => {
-    const finance = finances.find(f => f.id === id);
-    if (!finance) { return };
-
-    setFocusedFinance(finance);
-  }
-
-  const deleteFinance = async (id: number) => {
-    await onDelete(id);
-    router.refresh();
-  }
-
-  const submitForm = async (values: FinanceFormValues) => {
-    if (values.id) {
-      // Edit existing finance
-      await onEdit(values.id, values as Omit<Finance, "id">);
-    } else {
-      await onSubmit(values as Omit<Finance, "id">);
-    }
-  }
-
-  return <>
-    <h1 className="text-lg font-semibold text-gray-900 sm:text-xl dark:text-gray-50">
-      Finance Details
-    </h1>
-    <div className="mt-4 sm:mt-6 lg:mt-10">
-      <div className="mb-4 w-full flex items-center justify-end">
-        <FinanceFormModal
-          initialValues={focusedFinance}
-          onSubmit={async values => {
-            await submitForm(values);
-          }}
-          title={focusedFinance && (focusedFinance as Finance).id ? "Edit finance" : "Add finance"}
-          description="Use the form below to manage your finances."
-        >
-          <Button onClick={() => { setFocusedFinance({}); }} className="mt-4 w-full gap-2 sm:mt-0 sm:w-fit">
-            <RiAddLine className="-ml-1 size-4 shrink-0" aria-hidden="true" />
-            Add finance
-          </Button>
-        </FinanceFormModal>
-      </div>
-      <FinanceDataTable onDeleteFinanceAction={deleteFinance} onFocusFinanceAction={onFocusFinance} finances={finances} />
-    </div>
-  </>
 }
--- src/app/(protected)/(main)/finance/page.tsx
+++ src/app/(protected)/(main)/finance/page.tsx
@@ -1,16 +1,73 @@
-import { createFinance, deleteFinance, fetchAllFinances, updateFinance } from "@/services/financeService";
-import FinancePage from "./FinancePage";
+"use client";
 
-export const dynamic = "force-dynamic";
+import { Button } from "@/components/Button";
+import { FinanceFormModal } from "@/components/ui/finances/FinanceFormModal";
+import FinanceDataTable from "@/components/ui/finances/financeDataTable";
+import { createFinance, fetchAllFinances, updateFinance } from "@/services/financeService";
+import { Finance, FinanceFormValues } from "@/types/finance.types";
+import { RiAddLine } from "@remixicon/react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
-export default async function Page() {
-  const finances = await fetchAllFinances();
+export default function Page() {
+  const router = useRouter();
+
+  const [finances, setFinances] = useState<Finance[] | null>(null);
+  const [focusedFinance, setFocusedFinance] = useState<FinanceFormValues | {}>({});
+
+  const onFocusFinance = (id: number) => {
+    const finance = finances?.find(f => f.id === id);
+    if (!finance) { return };
+
+    setFocusedFinance(finance);
+  }
+
+  const deleteFinance = async (id: number) => {
+    await deleteFinance(id);
+    router.refresh();
+  }
+
+  const submitForm = async (values: FinanceFormValues) => {
+    if (values.id) {
+      // Edit existing finance
+      await updateFinance(values.id, values as Omit<Finance, "id">);
+    } else {
+      await createFinance(values as Omit<Finance, "id">);
+    }
+
+    fetchFinanceData();
+  }
+
+  const fetchFinanceData = async () => {
+    const data = await fetchAllFinances();
+    setFinances(data);
+  }
+
+  useEffect(() => {
+    fetchFinanceData();
+  }, []);
 
   return <>
-    <FinancePage
-      finances={finances}
-      onEdit={updateFinance}
-      onDelete={deleteFinance}
-      onSubmit={createFinance} />
+    <h1 className="text-lg font-semibold text-gray-900 sm:text-xl dark:text-gray-50">
+      Finance Details
+    </h1>
+    <div className="mt-4 sm:mt-6 lg:mt-10">
+      <div className="mb-4 w-full flex items-center justify-end">
+        <FinanceFormModal
+          initialValues={focusedFinance}
+          onSubmit={async values => {
+            await submitForm(values);
+          }}
+          title={focusedFinance && (focusedFinance as Finance).id ? "Edit finance" : "Add finance"}
+          description="Use the form below to manage your finances."
+        >
+          <Button onClick={() => { setFocusedFinance({}); }} className="mt-4 w-full gap-2 sm:mt-0 sm:w-fit">
+            <RiAddLine className="-ml-1 size-4 shrink-0" aria-hidden="true" />
+            Add finance
+          </Button>
+        </FinanceFormModal>
+      </div>
+      <FinanceDataTable onDeleteFinanceAction={deleteFinance} onFocusFinanceAction={onFocusFinance} finances={finances || []} />
+    </div>
   </>
 }
